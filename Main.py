c7ec27652e70b14566545b4e9d029c1cd560b610
Yogesh Vishwakarma <yogeshv1603@gmail.com>
2024-05-30 20:51:03 +0530

    added main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+#class defining
+class MatOps:
+    # __init__ ---> Constructor-->that execute the method as soon as the object of the class is created
+    def __init__(self):
+        self.mat1=list()
+        self.mat2=list()
+    def mat(self):
+
+
+
