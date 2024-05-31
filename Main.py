70cf09ab025d10aea6557224e79db01b1fc9e614
Yogesh Vishwakarma <yogeshv1603@gmail.com>
2024-05-31 21:54:49 +0530

    multiplication is modified

--- Main.py
+++ Main.py
@@ -5,34 +5,35 @@ class Matrix:
     #self is object and we can say object is self
     #instance variable--> the variable for which the value of that is different for different objects
 
-    def __init__(self,mat):
-        self.mat=mat
+    def __init__(self, mat):
+        self.mat = mat
         self.__rows = len(mat)
         self.__cols = len(mat[0])
 
     #__str__ --> special method which defines how the data type looks like
     def __str__(self):
-        res=""
+        res = ""
         for i in range(len(self.mat)):
             for j in range(len(self.mat[i])):
-                res+=str(self.mat[i][j])+" "
-            res+="\n"
+                res += str(self.mat[i][j]) + " "
+            res += "\n"
         return res
 
-    def __add__(self,other):
+    def __add__(self, other):
         if self.__rows != other.__rows or self.__cols != other.__cols:
             raise ValueError("Matrices must have same number of rows and columns")
         else:
             for i in range(len(self.mat)):
                 for j in range(len(self.mat[i])):
-                        self.mat[i][j]+=other.mat[i][j]
+                    self.mat[i][j] += other.mat[i][j]
             res = ""
             for i in range(len(self.mat)):
                 for j in range(len(self.mat[i])):
                     res += str(self.mat[i][j]) + " "
                 res += "\n"
             return res
-    def __sub__(self,other):
+
+    def __sub__(self, other):
         if self.__rows != other.__rows or self.__cols != other.__cols:
             raise ValueError("Matrices must have same number of rows and columns")
         else:
@@ -46,10 +47,25 @@ class Matrix:
                 res += "\n"
             return res
 
+    def __mul__(self, other):
+        if self.__cols != other.__rows:
+            raise ValueError("Matrix multiplication is not possible")
+        else:
+            #bug here
+            result_data = []
+            for i in range(self.__rows):
+                result_row = []
+                for j in range(other.__cols):
+                    sum_product = 0
+                    for k in range(self.__cols):
+                        sum_product += self.mat[i][k] * other.mat[k][j]
+                    result_row.append(sum_product)
+                result_data.append(result_row)
+            return Matrix(result_data)
 
-Mat1=Matrix([[1,3,3,5],[1,2,3,4]])
-Mat2=Matrix([[1,3,5,7],[2,3,4,5]])
+Mat1 = Matrix([[1,1], [1, 1]])
+Mat2 = Matrix([[1, 1], [1,1,1,1]])
 
-print(Mat1)
-print(Mat2)
-print(Mat2-Mat1)
+
+print(Mat1 * Mat2)
+
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # MatOps-Library
 This library create a new data type named Matrix which will be able to perform the matrix operation
 
+# Operations Implemented
+1. addition
+2. subtraction
+3. multiplication --> optmization needed 
+
 # Encapsulated variable
 the variable that are encapsulated are
 1. rows
