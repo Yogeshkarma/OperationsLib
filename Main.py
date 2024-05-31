f8cef76e478c8b8a3c83c45741dbaeaffc29212b
Yogesh Vishwakarma <yogeshv1603@gmail.com>
2024-05-31 18:12:00 +0530

    some variable are encapsulated

--- Main.py
+++ Main.py
@@ -3,11 +3,12 @@ class Matrix:
     # __init__ ---> Constructor-->that execute the method as soon as the object of the class is created
     # special/magic/dunder method
     #self is object and we can say object is self
+    #instance variable--> the variable for which the value of that is different for different objects
 
     def __init__(self,mat):
         self.mat=mat
-        self.rows = len(mat)
-        self.cols = len(mat[0])
+        self.__rows = len(mat)
+        self.__cols = len(mat[0])
 
     #__str__ --> special method which defines how the data type looks like
     def __str__(self):
@@ -19,13 +20,36 @@ class Matrix:
         return res
 
     def __add__(self,other):
-        if self.rows != other.rows or self.cols != other.cols:
+        if self.__rows != other.__rows or self.__cols != other.__cols:
             raise ValueError("Matrices must have same number of rows and columns")
         else:
             for i in range(len(self.mat)):
                 for j in range(len(self.mat[i])):
                         self.mat[i][j]+=other.mat[i][j]
+            res = ""
+            for i in range(len(self.mat)):
+                for j in range(len(self.mat[i])):
+                    res += str(self.mat[i][j]) + " "
+                res += "\n"
+            return res
+    def __sub__(self,other):
+        if self.__rows != other.__rows or self.__cols != other.__cols:
+            raise ValueError("Matrices must have same number of rows and columns")
+        else:
+            for i in range(len(self.mat)):
+                for j in range(len(self.mat[i])):
+                    self.mat[i][j] -= other.mat[i][j]
+            res = ""
+            for i in range(len(self.mat)):
+                for j in range(len(self.mat[i])):
+                    res += str(self.mat[i][j]) + " "
+                res += "\n"
+            return res
+
 
 Mat1=Matrix([[1,3,3,5],[1,2,3,4]])
 Mat2=Matrix([[1,3,5,7],[2,3,4,5]])
-print(Mat1+Mat2)
+
+print(Mat1)
+print(Mat2)
+print(Mat2-Mat1)
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # MatOps-Library
 This library create a new data type named Matrix which will be able to perform the matrix operation
 
+# Encapsulated variable
+the variable that are encapsulated are
+1. rows
+2. cols
 
+If you want to access it outside the class just give 
+``_classname__varname``
+>Please note that nothing in the pythonic world is private 
